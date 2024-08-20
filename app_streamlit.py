e5de6128c3647c6c259de6475c12fd94001ccaf8
unknown <wasimmadha@gmail.com>
2024-08-20 14:57:41 +0530

    final_vesion_without_maskign

--- app_streamlit.py
+++ app_streamlit.py
@@ -10,152 +10,222 @@ warnings.filterwarnings('ignore')
 # Load data
 per_audio_df = pd.read_csv("per_audio1.csv")
 summary_df = pd.read_csv("summary1.csv", index_col=0)
+transcribe = pd.read_csv("transcription.csv")
+categories = list(summary_df.columns)
+
+def submitted():
+    st.session_state.submitted = True
+
+def update_category(category):
+    st.session_state.selected_category = category
+
+def update_subcategory(subcategory):
+    st.session_state.selected_subcategory = subcategory
+
+analysis_type = ["Summarized", "Individual Audio"]
+time_frames = ["April-May"]
+subctegories = ["Questions", "Concerns", "Emotions", "Interest", "Transcript"]
 
 # Initialize session state
 if 'selected_category' not in st.session_state:
-    st.session_state.selected_category = summary_df.columns[0]
+    st.session_state.selected_category = None
+if 'selected_subcategory' not in st.session_state:
+    st.session_state.selected_subcategory = None
+if 'submitted' not in st.session_state:
+    st.session_state.submitted = False
 if 'selected_time_frame' not in st.session_state:
-    st.session_state.selected_time_frame = "April-May"
-if 'selected_subsection' not in st.session_state:
-    st.session_state.selected_subsection = "Summary"  # Default to Summary
+    st.session_state.selected_time_frame = time_frames[0]
+if 'selected_analysis_type' not in st.session_state:
+    st.session_state.selected_analysis_type = analysis_type[0]  # Default to Summary
 if 'selected_audio' not in st.session_state:
-    st.session_state.selected_audio = None
+    st.session_state.selected_audio = per_audio_df['Audio Name'].unique()[0]
 
 def main():
-    st.title("Real Estate Audio Analysis")
+    st.set_page_config(layout="wide")  
 
-    modal = Modal(
-        "Demo Modal", 
-        key="demo-modal",
-        
-    )
-    time_frames = ["April-May"]
-    col1, _, _ = st.columns([1, 1, 1])  # Define three columns with equal width
-    with col1:  # Select the second column for the select box
-        selected_time_frame = st.selectbox(
-            "Select Time Frame",
-            time_frames,
-            index=time_frames.index(st.session_state.selected_time_frame),
-            on_change=lambda: st.session_state.update(
-                selected_time_frame=st.session_state["selectbox_time_frame"]
-            ),
-            key="selectbox_time_frame"
-        )
+    content = "Real-estate Audio Analysis"
+    # Apply CSS styling to remove margin and padding and set the color to green
+    styled_component = f'<div style="margin: 0; padding: 1 0; color: #013208; font-size: 2rem">{content}</div>'
 
-    # Custom CSS for buttons
+    # Display the styled component using st.markdown
+    st.markdown(styled_component, unsafe_allow_html=True)
+
+    # Inject custom CSS
     st.markdown(
         """
         <style>
-        .section-button>button {
-            background-color: #ADD8E6; /* Light Blue */
-            border: none;
-            color: white;
-            padding: 15px 32px;
+        /* Change the width of the sidebar */
+            [data-testid="stSidebar"] {
+                width: 300px;  /* Adjust the width as needed */
+            }
+            
+            /* Adjust the main content width to account for the wider sidebar */
+            [data-testid="stSidebar"] + div {
+                left: 300px;  /* Same as the sidebar width */
+        }
+
+        .st-emotion-cache-6qob1r {
+            position: relative;
+            width: 300px;
+            overflow: overlay;
+        }
+
+        .st-emotion-cache-dvne4q {
+            padding: 3rem 1.5rem;
+        }
+
+        .button {
+            font-size: 16px !important;
+        }
+        .special-button {
+            font-size: 20px !important;
+        }
+
+        .st-emotion-cache-q3uqly {
+            font-size: 13px !important;
+            background-color: #f0f0f0;
+            color: black;
+            border: 1px solid #ccc;
             text-align: center;
             text-decoration: none;
             display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
             cursor: pointer;
-            border-radius: 10px;
+            transition: background-color 0.3s ease;
         }
-        .section-button>button:hover {
-            background-color: #B0E0E6; /* Lighter Blue on Hover */
+
+        .st-emotion-cache-j6qv4b p {
+            font-size: 13px; /* Adjust the font size as needed */
+        }
+        .st-emotion-cache-13ejsyy {
+            display: inline-flex;
+            -webkit-box-align: center;
+            align-items: center;
+            -webkit-box-pack: center;
+            justify-content: center;
+            font-weight: 300;
+            font: 12px !important;
+            padding: 0rem 0.5rem;
+            border-radius: 0.5rem;
+            min-height: 38.4px;
+            margin: 0px;
+            line-height: 1.6;
+            color: inherit;
+            width: auto;
+            user-select: none;
+            background-color: rgb(249, 249, 251);
+            border: 1px solid rgba(49, 51, 63, 0.2);
+        }
+
+        .st-emotion-cache-1jicfl2 {
+            width: 100%;
+            padding: 3rem 1rem 10rem;
+            min-width: auto;
+            max-width: initial;
+        }
+    
+        .button:hover {
+            background-color: lightgreen;
             color: black;
-        }
-        .audio-button>button {
-            background-color: #FFA07A; /* Light Salmon */
-            border: none;
-            color: white;
-            padding: 8px 16px;  /* Smaller padding for smaller buttons */
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 14px;  /* Smaller font size */
-            margin: 4px 2px;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-        .audio-button>button:hover {
-            background-color: #FFA07A; /* Lighter Salmon on Hover */
-            color: black;
-        }
-        .scroll-container {
-            max-height: 400px;
-            overflow-y: auto;
         }
         </style>
         """,
         unsafe_allow_html=True
     )
 
-    categories = summary_df.columns
-    selected_category = st.selectbox(
-        "Select Category",
-        categories,
-        index=list(categories).index(st.session_state.selected_category),
-        on_change=lambda: st.session_state.update(
-            selected_category=st.session_state["selectbox_category"]
-        ),
-        key="selectbox_category"
-    )
+    selected_time = st.sidebar.selectbox("Select Time Period", time_frames, index=time_frames.index(st.session_state.selected_time_frame) if st.session_state.selected_time_frame else 0)
+    st.session_state.selected_time_frame = selected_time
+
+    if st.session_state.selected_analysis_type:
+        # Create columns for buttons
+        col1, col2 = st.sidebar.columns(2)
+        
+        for idx, category in enumerate(categories):
+            if idx % 2 == 0:
+                with col1:
+                    if st.button(category, type="primary"):
+                        update_category(category)
+            else:
+                with col2:
+                    if st.button(category, type="primary"):
+                        update_category(category)
+    
+    selected_analysis_type = st.sidebar.selectbox("Analysis Type", analysis_type)
+    st.session_state.selected_analysis_type = selected_analysis_type
+
+    if st.session_state.selected_analysis_type == 'Individual Audio':
+        audio_names = per_audio_df['Audio Name'].unique().tolist()
+        selected_name = st.sidebar.selectbox("Select Audio", audio_names)
+        st.session_state.selected_audio = selected_name
 
     # Create a big container
     big_container = st.container(height=500)
 
+    # Display buttons horizontally within the big container
     with big_container:
-        button_container = st.columns(6)  # Display buttons horizontally
+        # Create columns for buttons
+        subcategory_cols = st.columns(len(subctegories))
+        
+        # Track the index for two column layout
+        for subcategory, cols in zip(subctegories, subcategory_cols):
+            with cols:
+                if st.button(subcategory):
+                    update_subcategory(subcategory)
+    
+        if st.session_state.selected_analysis_type == "Summarized":
+            if st.session_state.selected_subcategory == 'Transcript':
+                st.warning("No Transcript for Summarized Analysis")
+                pass
+            else:
+                try:
+                    sb_df = summary_df[summary_df.index == st.session_state.selected_subcategory][st.session_state.selected_category]
+                    if len(sb_df) == 0:
+                        if not st.session_state.selected_category and st.session_state.selected_subcategory:
+                            st.warning("Please Select Category and Subcategory")
+                        elif not st.session_state.selected_category:
+                            st.warning("Please select Category ")
 
-        # Define variables to track button states
-        button1_state = button_container[0].button(
-            "Audio",
-            key="button1",
-            on_click=lambda: update_button_state("Audio")
-        )
-        button2_state = button_container[1].button(
-            "Summary",
-            key="button2",
-            on_click=lambda: update_button_state("Summary")
-        )
-
-        selected_subsection = st.session_state.selected_subsection
-
-        if selected_subsection == "Summary":
-            sb_df = summary_df[[selected_category]]  # Select the column as a DataFrame
-            st.markdown("<div class='scroll-container'>", unsafe_allow_html=True)
-            for index, row in sb_df.iterrows():
-                with st.expander(index):
-                    summary_points = ast.literal_eval(row[selected_category])['Summary']
+                    summary_points = ast.literal_eval(sb_df.iloc[0])['Summary']
+                    st.write(f"Summarized {st.session_state.selected_subcategory} for {st.session_state.selected_category}:")
                     for point in summary_points:
                         st.write(f"- {point.strip()}")
-            st.markdown("</div>", unsafe_allow_html=True)
-        elif selected_subsection == "Audio":
-            # Get unique audio names
-            audio_names = per_audio_df['Audio Name'].unique().tolist()
-            
-            # Create two columns for the audio buttons
-            col1, col2 = st.columns([1, 4])
-            
-            # Distribute the audio names into two columns
-            for i, audio_name in enumerate(audio_names):
-                # if i % 2 == 0:
-                #     if col1.button(audio_name, key=f"audio_{i}", type="secondary"):
-                #         st.session_state.selected_audio = audio_name
-                # else:
-                if col1.button(audio_name, key=f"audio_{i}", type="secondary"):
-                    st.session_state.selected_audio = audio_name
-            
-            with col2:
-                if st.session_state.selected_audio != None:
-                    audio = per_audio_df[(per_audio_df["Audio Name"] == st.session_state.selected_audio) & (per_audio_df["Subcategory"] == selected_category)]
-                    for idx, column in enumerate(['Questions', 'Concerns', 'Emotions','Preferences']):
-                        with st.expander(column):
-                            summary_points = ast.literal_eval(audio[column].iloc[0])
-                            for point in summary_points:
-                                st.write(f"- {point.strip()}")
-            st.markdown("</div>", unsafe_allow_html=True)
+                except:
+                    st.warning("Please select Sub Category")
 
-        print(st.session_state.selected_audio)
+        elif st.session_state.selected_analysis_type == 'Individual Audio':
+            if st.session_state.selected_subcategory == 'Transcript':
+                transcription = transcribe[transcribe['audio_name'] == st.session_state.selected_audio]['transcription'].iloc[0]
+                st.write(transcription)
+            else:
+                if not st.session_state.selected_category and st.session_state.selected_subcategory:
+                    st.warning("Please Select Category and Subcategory")
+                elif not st.session_state.selected_category:
+                    st.warning("Please select Category ")
+                elif not st.session_state.selected_subcategory:
+                    st.warning("Please select SubCategory")
+                else:
+                    st.write(f"{st.session_state.selected_subcategory} for {st.session_state.selected_category}:")
+
+                    if st.session_state.selected_category == 'All':
+                        audio_insights = transcribe[transcribe['audio_name'] == st.session_state.selected_audio]['Insights_new__'].iloc[0]
+                        print(ast.literal_eval(audio_insights).keys())
+                        if st.session_state.selected_subcategory == 'Interest':
+                            summary_points = ast.literal_eval(audio_insights)['Preferences']
+                        else:
+                            summary_points = ast.literal_eval(audio_insights)[st.session_state.selected_subcategory]
+                    else:
+                        audio_df = per_audio_df[(per_audio_df["Audio Name"] == st.session_state.selected_audio) & (per_audio_df["Subcategory"] == st.session_state.selected_category)]
+                        # summary_points = ast.literal_eval(audio_df[st.session_state.selected_subcategory].iloc[0])
+                        print(audio_df.columns)
+                        try:
+
+                            if st.session_state.selected_subcategory == 'Interest':
+                                summary_points = ast.literal_eval(audio_df['Preferences'].iloc[0])
+                            else:
+                                summary_points = ast.literal_eval(audio_df[st.session_state.selected_subcategory].iloc[0])
+                        except:
+                            st.warning(f"No {st.session_state.selected_subcategory} in {st.session_state.selected_category} for {st.session_state.selected_audio}")
+                    for point in summary_points:
+                        st.write(f"- {point.strip()}")
 
 def update_button_state(subsection):
     st.session_state.selected_subsection = subsection
